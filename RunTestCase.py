bb8a4145aa97fb1b2e8a51f5f7afcadc916abf3a
13691579846 <281754043@qq.com>
2019-04-18 15:06:57 +0800

    add main function for run all

--- RunTestCase.py
+++ RunTestCase.py
@@ -7,11 +7,11 @@
 @Motto: Real warriors,dare to face the bleak warning,dare to face the incisive error!
 ------------------------------------
 """
+import sys
+from config.conf import *
+from util.sendMailForReprot import sendMailWithReport
 
-if __name__=='__main__':
-    import sys
-    from config.conf import *
-    from util.sendMailForReprot import sendMailWithReport
+def main():
     # 判断项目的根目录是否在sys.path中，没有就添加
     if projectDir not in sys.path:
         sys.path.append(projectDir)
@@ -21,3 +21,5 @@ if __name__=='__main__':
     sendMailWithReport.sendMail(smtpServer, fromUser, fromPassWord,
                                 toUser, subject, contents,
                               htmlName)
+if __name__=='__main__':
+    main()
