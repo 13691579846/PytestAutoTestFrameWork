d575a687526c625ac283e94520283dd15ede40a7
13691579846 <281754043@qq.com>
2019-04-19 13:17:33 +0800

    add login failed assert function

--- Page/PageObject/LoginPage.py
+++ Page/PageObject/LoginPage.py
@@ -20,6 +20,7 @@ frame = cf.getLocatorsOrAccount('LoginPageElements', 'frame')
 username = cf.getLocatorsOrAccount('LoginPageElements', 'username')
 password = cf.getLocatorsOrAccount('LoginPageElements', 'password')
 loginBtn = cf.getLocatorsOrAccount('LoginPageElements', 'loginBtn')
+ferrorHead = cf.getLocatorsOrAccount('LoginPageElements', 'ferrorHead') # 登录失败提示
 # ---------------------------------------------------------------------------------
 
 class LoginPage(BasePage):
@@ -37,9 +38,18 @@ class LoginPage(BasePage):
         self.switchToDefaultFrame()
         print('---------end login---------')
 
+    # add at 2019/04/19
+    def assertTextEqString(self, expected, name = None):
+        '''断言提示信息是否与期望的值相等'''
+        self.switchToFrame(*frame)
+        text = self.getElementText(*ferrorHead, name)
+        self.switchToDefaultFrame()
+        print('info: assert "{}" == "{}"'.format(text, expected))
+        assert text == expected, '{} ！= {}'.format(text, expected)
 
 if __name__=="__main__":
     from selenium import webdriver
     driver = webdriver.Firefox()
     login = LoginPage(driver, 30)
-    login.login('账号', 'xiaochao11520')
+    login.login('lin', '')
+    login.assertTextEqString('请输入密码')
