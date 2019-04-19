02b95f8f6b606ee33ab0e401b378b41bf0435d54
13691579846 <281754043@qq.com>
2019-04-19 13:11:04 +0800

    add login failed verify

--- TestCases/test_loginCase.py
+++ TestCases/test_loginCase.py
@@ -8,13 +8,16 @@
 ------------------------------------
 """
 import pytest
-from Page.PageObject.LoginPage import LoginPage
+from Page.PageObject.LoginPage import LoginPage, cf
 
 # ---------------------------------------------------------------------------------
 # 测试数据
-loginData = [('账号', 'xiaochao11520'),
-                ('账号', 'xiaochao11520'),
-                ('账号', 'xiaochao11520')]
+loginData = [('linux', ''),
+            ('', 'chao'),
+            ('l', 'chao'),
+            ('', '')]
+userName = cf.getLocatorsOrAccount('126LoginAccount', 'username')
+passWord = cf.getLocatorsOrAccount('126LoginAccount', 'password')
 # ---------------------------------------------------------------------------------
 
 @pytest.fixture()
@@ -33,7 +36,18 @@ def test_login(teardown_func, driver, username, password):
     login = LoginPage(driver, 30)
     login.login(username, password)
     login.sleep(5)
-    login.assertValueInSource(username)
+
+    # 增加登录失败时， 对提示信息的验证
+    if username == userName and password == passWord:
+        login.assertValueInSource('写 信')
+    elif username == '':
+        login.assertTextEqString('请输入帐号')
+    elif username != '' and password == '':
+        login.assertTextEqString('请输入密码')
+    elif username == '' and password == '':
+        login.assertTextEqString('请输入帐号')
+    else:
+        login.assertTextEqString('帐号或密码错误')
 
 if __name__=="__main__":
     pytest.main(['-v', 'test_loginCase.py'])
