25abc22afee9ed1e7b3f0b84bdaf331981f9153a
13691579846 <281754043@qq.com>
2019-04-22 16:10:52 +0800

    20190422 optimize code

--- TestCases/test_sendMailCase.py
+++ TestCases/test_sendMailCase.py
@@ -14,18 +14,18 @@ from Page.PageObject.SendMailPage import SendMailPage
 # 测试数据 0和1 标记是否发送附件
 # Address, Subject, Text
 mailData = [
-('281754043@qq.com', 'pytest测试主题1', 'pytest 测试邮件, 请勿回复!', 0, ''),
-('281754043@qq.com', 'pytest测试主题2', 'pytest 测试邮件, 请勿回复!', 1, 'D:\KeyWordDriverTestFrameWork\geckodriver.log'),
-('281754043@qq.com', 'pytest测试主题3', 'pytest 测试邮件, 请勿回复!', 1, 'D:\KeyWordDriverTestFrameWork\geckodriver.log'),
+('281754043@qq.com', 'pytest测试主题1', 'pytest 测试邮件, 请勿回复!', ''),
+('281754043@qq.com', 'pytest测试主题2', 'pytest 测试邮件, 请勿回复!', 'D:\KeyWordDriverTestFrameWork\geckodriver.log'),
+('281754043@qq.com', 'pytest测试主题3', 'pytest 测试邮件, 请勿回复!', 'D:\KeyWordDriverTestFrameWork\geckodriver.log'),
 ]
 # ---------------------------------------------------------------------------------
 
 @pytest.mark.sendmail
-@pytest.mark.parametrize('Address, Subject, Text, Flag, PFA', mailData)
-def test_sendMail(driver, login, Address, Subject, Text, Flag, PFA):
+@pytest.mark.parametrize('Address, Subject, Text, PFA', mailData)
+def test_sendMail(driver, login, Address, Subject, Text,PFA):
     '''测试发送邮件，包括带附件的邮件'''
     send_mail = SendMailPage(driver)
-    send_mail.sendMail(Address, Subject, Text, Flag, PFA)
+    send_mail.sendMail(Address, Subject, Text, PFA)
     send_mail.sleep(5)
     assert send_mail.isElementExsit(*SendMailPage.expect)
 
